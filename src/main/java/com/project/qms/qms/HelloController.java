7fd021be1fab629e931ee135e4ef97eea8b147ac
Sophia <sophie.aloria@gmail.com>
2025-08-24 16:57:26 +0800

    new code

--- src/main/java/com/project/qms/qms/HelloController.java
+++ src/main/java/com/project/qms/qms/HelloController.java
@@ -1,4 +1,5 @@
 package com.project.qms.qms;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,12 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
         return "hello world";
+        //comment in here <----
     }
 
-    
+    @GetMapping("/hi")
+    public String hi(){
+        return "hello world";   
+    }
+
 }
